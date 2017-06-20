eac877a3c6964a5ca96adda1c66c873800f3d139
Christopher Rozario <christopher.rosari.old@buscapecompany.com>
2017-06-20 15:37:50 -0300

    initial

---
+++ src/main/java/com/gamaset/caliban/App.java
@@ -0,0 +1,13 @@
+package com.gamaset.caliban;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
